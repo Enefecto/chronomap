3cfb21c7a96617f6fc341d9cd811b1c259dccb2d
Camilo Romero <camids980@gmail.com>
2024-10-02 13:55:27 -0300

    feat: api geonames integrated, and 50 markers of the most important cities added

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,19 +1,35 @@
+import { useEffect, useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
-const MapComponent = () => {
+
+const MapComponent = ({apiUsername}) => {
+  const [cities, setCities] = useState([]);
+  useEffect(() => {
+    const fetchCities = async () => {
+      const response = await fetch(
+        `http://api.geonames.org/searchJSON?country=CL&featureClass=P&maxRows=50&orderby=population&username=${apiUsername}`
+      );
+      const data = await response.json();
+      setCities(data.geonames);
+    };
+
+    fetchCities();
+  }, []);
+
   return (
-    <MapContainer center={[-33.4489, -70.6693]} zoom={6} className="w-full h-[500px]">
+    <MapContainer center={[-33.4489, -70.6693]} zoom={5} className="w-full h-[500px]">
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
-      <Marker position={[-33.4489, -70.6693]}>
-        <Popup>
-          Santiago de Chile<br /> Capital del país.
-        </Popup>
-      </Marker>
-      {/* Puedes agregar más marcadores con diferentes coordenadas */}
+      {cities.map((city, index) => (
+        <Marker key={index} position={[city.lat, city.lng]}>
+          <Popup>
+            {city.name}
+          </Popup>
+        </Marker>
+      ))}
     </MapContainer>
   );
 };
